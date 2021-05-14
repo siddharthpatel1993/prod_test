15c81733c51c3648b18f419038d58549f4a0531e
sidd <sidd@gmail.com>
2021-05-14 05:04:19 +0000

    commit 5

--- script/connectivity_test.py
+++ script/connectivity_test.py
@@ -21,15 +21,41 @@ def copy_config():
         #print(a1)
         subprocess.check_output("mkdir -p ~/."+ rel_name +"", shell=True)
         subprocess.check_output("cp "+ a1 +" ~/."+ rel_name +"", shell=True)
+    main_func()
 
 def stop_service():
     subprocess.call("service apache2 stop", shell=True)
     subprocess.call("service apache2 status", shell=True)
+    main_func()
 
 def deploy_service():
-    subprocess.check_output("cp") 
+    file1 = open('../configuration_and_log/configuration.txt', 'r')
+    Lines = file1.readlines()
+    file1.close()
 
-def url_service_function1():
+    count = 0
+    #Strips the newline character
+    for line in Lines:
+        count = count + 1
+        host_num = count
+
+        a1 = "{}".format(line.strip())
+        #print(a1)
+
+        s0 = re.compile(r'/[a-z]+[.][a-z]+')
+        m0 = s0.search(a1)
+        #print(m0.group())
+        a0=m0.group().lstrip('/')
+
+        print("******Works start**********")
+        print("Copying ../main_conf/"+ a0 +" and "+ a1 +"")
+        subprocess.check_output("cp ../main_conf/"+ a0 +" "+ a1 +"", shell=True)
+        print("******Done*********")
+        time.sleep(3)
+    print("*****Completed*****")
+    main_func()
+
+def compare_config():
     # Using readlines()
     file1 = open('../configuration_and_log/configuration.txt', 'r')
     Lines = file1.readlines()
@@ -56,26 +82,26 @@ def url_service_function1():
         time.sleep(3)
 
     print("********Completed the Work***********")
+    main_func()
 
-def url_service_function2():
+def check_checksum():
+    main_func()
 
-    # Using readlines()
-    file1 = open('configuration_and_log/configuration.txt', 'r')
-    Lines = file1.readlines()
-    file1.close()
+def start_service():
+    subprocess.call("service apache2 start", shell=True)
+    subprocess.call("service apache2 status", shell=True)
+    main_func()
 
-    count = 0
-    #Strips the newline character
-    for line in Lines:
-        count = count + 1
-        host_num = count
+def check_alarm():
+    a = subprocess.call("grep -i -E 'error|alarm' /var/log/apache2/error.log", shell=True)
+    print(a)
+    main_func()
 
-        a1 = "{}".format(line.strip())
-        #print(a1)
+def Notify_everyone():
+    main_func()
 
-        subprocess.check_output("cp "+ a1 +" .config/", shell=True)
-
-print('''enter 1 to copy the config files
+def main_func():
+    print('''enter 1 to copy the config files
 enter 2 to stop the service
 enter 3 to deploy the service
 enter 4 to compare the config files
@@ -83,23 +109,26 @@ enter 5 see the checksum if that is ok
 enter 6 to start the app
 enter 7 check the alarm
 enter 8 to notify everyone throgh email''')
-a = int(input())
-if (a==1):
-    copy_config()
-elif(a==2):
-    stop_service()
-elif(a==3):
-    deploy_service()
-elif(a==4):
-    compare_config()
-elif(a==5):
-    check_checksum() 
-elif(a==6):
-    start_app()
-elif(a==7):
-    check_alarm()
-elif(a==8):
-    Notify_everyone()
-else:
-    print("Wrong choice")
+    a = int(input())
+    if (a==1):
+        copy_config()
+    elif(a==2):
+        stop_service()
+    elif(a==3):
+        deploy_service()
+    elif(a==4):
+        compare_config()
+    elif(a==5):
+        check_checksum() 
+    elif(a==6):
+        start_service()
+    elif(a==7):
+        check_alarm()
+    elif(a==8):
+        Notify_everyone()
+    else:
+        print("Wrong choice")
+        #break
+
+main_func()
 
